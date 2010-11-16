f1eb2bb5e91d692085fed8c2104b015bc71c2d2c
Bob Copeland <me@bobcopeland.com>
2010-11-15 21:25:39 -0500

    Update the graph when the degree is wrong

--- java/walker/Streamer.java
+++ java/walker/Streamer.java
@@ -2,22 +2,35 @@ package walker;
 
 public class Streamer implements StreamHandler
 {
-    public Streamer()
+    private Crawl crawl;
+
+    public Streamer(GraphDB db)
     {
+        crawl = new Crawl(db);
     }
 
     public void onItem(String identifier, Object object)
     {
+        if (object instanceof GraphUpdate)
+        {
+            try {
+                crawl.update(((GraphUpdate) object).getId());
+            } catch (Exception e) {
+                System.err.println(e);
+            }
+        }
+
         System.out.println("item: " + object);
     } 
 
     public static void main(String[] args)
         throws Exception
     {
+        GraphDB db = new HibernateGraphDB();
         TwitterStream ts = new TwitterStream();
-        GraphStream gs = new GraphStream(ts, new HibernateGraphDB());
+        GraphStream gs = new GraphStream(ts, db);
 
-        Streamer streamer = new Streamer();
+        Streamer streamer = new Streamer(db);
 
         gs.registerHandler(streamer);
         ts.registerHandler(streamer);
