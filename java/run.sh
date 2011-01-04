8b0fe52f4e20a782aee5b96475f40a220571d18e
Bob Copeland <me@bobcopeland.com>
2011-01-04 14:14:40 -0500

    Don't really need that much memory

--- java/run.sh
+++ java/run.sh
@@ -6,4 +6,4 @@ for i in lib/*jar; do
     CP=$CP:$i:/usr/share/java/postgresql.jar
 done
 
-java -Xmx1024M -Xms1024M -cp $CP walker.RandWalk "$@"
+java -Xmx512M -Xms512M -cp $CP walker.RandWalk "$@"
